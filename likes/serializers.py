d14550123391ceaa60866496e23e8101a8efa0db
Hampus Hjortsberg <hampushjortsberg@gmail.com>
2024-06-13 22:38:32 +0000

    Create likes app/ Add model and serializer

---
+++ likes/serializers.py
@@ -0,0 +1,17 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from likes.models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'recipe']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'duplicate like'})
--- tastehub_drf/settings.py
+++ tastehub_drf/settings.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'profiles',
-    'recipes'
+    'recipes',
+    'likes',
 ]
 
 MIDDLEWARE = [
