11b1108da07e3a4c24c8e8e66d9430f3daa9ef85
Hampus Hjortsberg <hampushjortsberg@gmail.com>
2024-06-13 22:48:27 +0000

    Add url paths for like app and LikeDetail view

--- tastehub_drf/urls.py
+++ tastehub_drf/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('', include('recipes.urls')),
+    path('', include('likes.urls')),
 ]
