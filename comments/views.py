1da26f0a2d335cd53b42aac2feecae7b8d1d9d4b
Hampus Hjortsberg <hampushjortsberg@gmail.com>
2024-06-14 01:36:09 +0000

    Add filters to comments

--- comments/views.py
+++ comments/views.py
@@ -2,12 +2,15 @@ from rest_framework import generics, permissions
 from tastehub_drf.permissions import IsOwnerOrReadOnly
 from comments.models import Comment
 from comments.serializers import CommentSerializer, CommentDetailSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['recipe']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
