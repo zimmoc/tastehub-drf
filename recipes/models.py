16b4b7ea27486613406c409f26eae600a5759f74
Hampus Hjortsberg <hampushjortsberg@gmail.com>
2024-06-17 06:32:26 +0000

    Fix Integrityerror caused by OneToOnefield on Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Recipe(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
